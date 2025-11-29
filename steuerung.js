8c81464673965f7f887b4ba4d1a4904abd5684e9
Marius Müller <marius@bscnet.de>
2025-11-29 23:01:46 +0100

    fix printing dialog issues

--- steuerung.js
+++ steuerung.js
@@ -400,6 +400,27 @@ function startPrintAndRedirect() {
     overlay.hidden = false;
   }
 
+  document.querySelector('style').textContent =
+  `@media print {
+      * { visibility: hidden;
+          margin: 0;
+          padding: 0;}
+      img {
+        visibility: visible;
+        position: absolute;
+        top: 0px;
+        left: 0px;
+        transform:translateY(-100%) rotate(90deg);
+      transform-origin: left bottom;
+        display: block;
+        width: 120px;
+        height: 212px;}
+    }`;
+
+  if (window.print) {
+    window.print();
+  }
+
   // Druck-Sound abspielen
   playPrintSound();
 
